6c57133f09f994edbafe001c17e0a181f50ca2cc
rahul <vkrahulagarwal@gmail.com>
2017-09-19 02:53:10 +0530

    updated sql for creating a player and the stored procedure for the same

--- civicq.sql
+++ civicq.sql
@@ -1,25 +1,27 @@
-CREATE TABLE `civicq`.`players` (
-  `user_id` BIGINT AUTO_INCREMENT,
+drop table players;
+
+CREATE TABLE players (
+  `id` BIGINT AUTO_INCREMENT,
   `name` VARCHAR(45),
   `reg_no` VARCHAR(45),
-  `user_email` VARCHAR(45),
-  `user_password` VARCHAR(200),
+  `email` VARCHAR(45),
+  `password` VARCHAR(200),
   `college` VARCHAR(20),
-  -- `curr_round` BIGINT(20),
-  `curr_ques_id` VARCHAR(20),
+  -- `curr_round` playersBIGINT(20),
+  `curr_ques_id` VARCHAR(20), -- xx_yy format xx corresponds to the round number and yy corresponds to the ques no.
   `r1_res` VARCHAR(20),
   `r2_res` VARCHAR(20),
   `r3_res` VARCHAR(20),
   `r4_res` VARCHAR(20),
   `r5_res` VARCHAR(20),
   `r6_res` VARCHAR(20),
-  `curr_trial` INT(5) -- tells which trial is he currently on. 0 for fresh
-  PRIMARY KEY (`user_id`));
+  `curr_trial` INT(5), -- tells which trial is he currently on. 0 for fresh
+  PRIMARY KEY (`id`));
 
 CREATE TABLE `civicq`.`score` (
   `user_id` BIGINT REFERENCES `players` (`user_id`),
   `points` BIGINT(20),
-  `pres_ques_id` VARCHAR(20),
+  `pres_ques_id` VARCHAR(20)
   );
 
 CREATE TABLE `civicq`.`questions` (
@@ -28,43 +30,25 @@ CREATE TABLE `civicq`.`questions` (
   `op1` VARCHAR(300),
   `op2` VARCHAR(300),
   `op3` VARCHAR(300),
-  `op4` VARCHAR(300)
+  `op4` VARCHAR(300),
   
   PRIMARY KEY (`ques_id`));
 
 
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `createUser`(
-    IN p_name VARCHAR(45),
-    IN p_username VARCHAR(45),
-    IN p_email VARCHAR(45),
-    IN p_password VARCHAR(200)
-)
-BEGIN
-    if ( select exists (select 1 from tbl_user where user_name = p_username) ) THEN
-     
-        select 'Username Exists !!';
-     
-    ELSE
-     
-        insert into tbl_user
-        (
-            name,
-            user_name,
-            user_email,
-            user_password
-        )
-        values
-        (
-            p_name,
-            p_username,
-            p_email,
-            p_password
-        );
-     
-    END IF;
-END$$
-DELIMITER ;
+delimiter $$
+create procedure `insert_player`(  IN p_name VARCHAR(45),    IN p_regno VARCHAR(45), IN p_email VARCHAR(45), IN p_password VARCHAR(200), in p_college varchar(20))
+begin
+	insert into players(name,reg_no,email,password,college,curr_ques_id,r1_res,r2_res,r3_res,r4_res,r5_res,r6_res,curr_trial)
+		values ( p_name,p_regno,p_email,p_password,p_college,'01_01',0,0,0,0,0,0,0);
+end$$
+delimiter ;
+
+call insert_player('rahul','150911112','abc@gmail.com','rahul123','MIT');
+
+select * from players;
+
+
+
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `validateLogin`(
