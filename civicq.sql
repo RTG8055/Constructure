a26c19122ab870c30573123e3bac6f43a30e63cf
Rahul <rahul.agarwal4@learner.manipal.edu>
2017-10-02 00:59:22 +0530

    update sql

--- civicq.sql
+++ civicq.sql
@@ -12,12 +12,27 @@ CREATE TABLE players (
   `curr_ques_id` VARCHAR(20), -- xx_yy format xx corresponds to the round number and yy corresponds to the ques no.
   `r1_res` VARCHAR(20),
   `r2_res` VARCHAR(20),
-  `r3_res` VARCHAR(20),
+  `r31_res` VARCHAR(20),
+  `r32_res` VARCHAR(20),
   `r4_res` VARCHAR(20),
   `r5_res` VARCHAR(20),
   `r6_res` VARCHAR(20),
   `curr_trial` INT(5), -- tells which trial is he currently on. 0 for fresh
   PRIMARY KEY (`id`));
+
+create table questions
+(
+  ques_id varchar(10) primary Key,
+    question varchar(1000),
+    op1 varchar(500),
+    op2 varchar(500),
+    op3 varchar(500),
+    op4 varchar(500),
+    ans varchar(10),
+    point_wt varchar(10),
+    moeny_wt varchar(10)
+    
+);
 
 drop table civicq.score;
 CREATE TABLE `civicq`.`score` (
@@ -26,15 +41,6 @@ CREATE TABLE `civicq`.`score` (
   `money` varchar(30)
   );
 
-CREATE TABLE `civicq`.`questions` (
-  `ques_id` VARCHAR(20),
-  `question` VARCHAR(400),
-  `op1` VARCHAR(300),
-  `op2` VARCHAR(300),
-  `op3` VARCHAR(300),
-  `op4` VARCHAR(300),
-  
-  PRIMARY KEY (`ques_id`));
 
 ---CREATE PLAYER PROCEDURE
 ---SIGNS UP A USER IF NOT ALREADY EXISTS
@@ -47,7 +53,7 @@ begin
 	if exists( SELECT ID FROM players WHERE  reg_no = p_regno) 
     then select 'Not unique';
 	else
-	insert into players(name,reg_no,email,mobile,password,college,ques_asked,curr_ques_id,r1_res,r2_res,r3_res,r4_res,r5_res,r6_res,curr_trial) values ( p_name,p_regno,p_email,p_mobile,p_password,p_college,'0','01_01',0,0,0,0,0,0,0);
+	insert into players(name,reg_no,email,mobile,password,college,ques_asked,curr_ques_id,r1_res,r2_res,r31_res,r32_res,r4_res,r5_res,r6_res,curr_trial) values ( p_name,p_regno,p_email,p_mobile,p_password,p_college,'0','01_01','0','0','0','0','0','0','0','0','0');
 	  end if;
 end$$
 delimiter ;
@@ -76,5 +82,10 @@ delimiter ;
 
 select @ans;
 
-create trigger 
+delimiter $$
+create trigger init_score after insert on players
+for each row
+insert into scores values (New.id,'0','0','0');
+$$
+delimiter ;
 
